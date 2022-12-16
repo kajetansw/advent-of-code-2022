7d1b880b40aa6f334755a58e0306724bd8701e47
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2022-12-16 15:04:03 +0100

    completed rust day 1 part 1

--- rust/src/aoc1/mod.rs
+++ rust/src/aoc1/mod.rs
@@ -1,3 +1,29 @@
+use std::fs;
+use std::io::Error;
+
 pub fn aoc1() -> (String, String) {
-  (String::from("Solution 1-1"), String::from("Solution 1-2"))
+  let input = get_input();
+  let calories_per_elf = input.map(input_to_calories);
+  let max_calorie_value = calories_per_elf
+    .map(|val| val.into_iter().max().unwrap_or(0))
+    .unwrap_or(0);
+
+  (max_calorie_value.to_string(), String::from("Solution 1-2"))
+}
+
+fn get_input() -> Result<String, Error> {
+  fs::read_to_string("src/aoc1/aoc1.txt")
+}
+
+fn input_to_calories(input: String) -> Vec<i64> {
+  input
+    .split("\n\n")
+    .into_iter()
+    .map(|per_elf| {
+      per_elf
+        .lines()
+        .map(|line| line.parse::<i64>().unwrap_or(0))
+        .sum()
+    })
+    .collect::<Vec<i64>>()
 }
