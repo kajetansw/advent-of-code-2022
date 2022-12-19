361264eeb2cd0e93f072f12d9910f1144bef115f
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2022-12-19 10:33:15 +0100

    completed purescript day 2 part 1

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -3,13 +3,16 @@ module Main where
 import Prelude
 
 import Aoc1 as Aoc1
+import Aoc2 as Aoc2
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
   aoc1Solution <- Aoc1.solution
+  aoc2Solution <- Aoc2.solution
   log $ logSolution 1 $ show aoc1Solution
+  log $ logSolution 2 $ show aoc2Solution
 
 logSolution :: Int -> String -> String
 logSolution n s = "Day #" <> show n <> ":\n" <> show s <> "\n"
---
+++ purescript/src/aoc2/Aoc2.purs
@@ -0,0 +1,88 @@
+module Aoc2
+  ( solution
+  ) where
+
+import Prelude
+
+import Data.Array (catMaybes, index)
+import Data.Foldable (sum)
+import Data.Maybe (Maybe(..))
+import Data.String (Pattern(..), split)
+import Data.Tuple (Tuple)
+import Data.Tuple.Nested ((/\))
+import Effect (Effect)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
+
+type Score =
+  { mine :: HandShape
+  , opponents :: HandShape
+  }
+
+data HandShape = Rock | Paper | Scissors
+
+instance eqHandShape :: Eq HandShape where
+  eq hs1 hs2 = case hs1 /\ hs2 of
+    Rock /\ Rock -> true
+    Paper /\ Paper -> true
+    Scissors /\ Scissors -> true
+    _ -> false
+
+instance orderHandShape :: Ord HandShape where
+  compare hs1 hs2 = case hs1 /\ hs2 of
+    Rock /\ Rock -> EQ
+    Rock /\ Paper -> LT
+    Rock /\ Scissors -> GT
+    Paper /\ Rock -> GT
+    Paper /\ Paper -> EQ
+    Paper /\ Scissors -> LT
+    Scissors /\ Rock -> LT
+    Scissors /\ Paper -> GT
+    Scissors /\ Scissors -> EQ
+
+solution :: Effect (Tuple Int Int)
+solution = do
+  i <- getInput
+  pure $ (sum $ inputToScoreCount i) /\ 1
+
+getInput :: Effect String
+getInput = readTextFile UTF8 "src/aoc2/aoc2.txt"
+
+inputToScoreCount :: String -> Array Int
+inputToScoreCount = split (Pattern "\n") >>> map toCode >>> map toScore >>> catMaybes >>> map countPoints
+
+toCode :: String -> Tuple String String
+toCode s = case scoreTuple of
+  Just o /\ Just m -> o /\ m
+  _ -> "" /\ ""
+  where
+  scoreTuple = (index (split (Pattern " ") s) 0) /\ (index (split (Pattern " ") s) 1)
+
+toScore :: Tuple String String -> Maybe Score
+toScore (s1 /\ s2) = case (toHandShape s1 /\ toHandShape s2) of
+  Just hs1 /\ Just hs2 -> Just { opponents: hs1, mine: hs2 }
+  _ -> Nothing
+  where
+  toHandShape :: String -> Maybe HandShape
+  toHandShape s = case s of
+    "A" -> Just Rock
+    "B" -> Just Paper
+    "C" -> Just Scissors
+    "X" -> Just Rock
+    "Y" -> Just Paper
+    "Z" -> Just Scissors
+    _ -> Nothing
+
+countPoints :: Score -> Int
+countPoints score =
+  let
+    countBonusPoints handShape = case handShape of
+      Rock -> 1
+      Paper -> 2
+      Scissors -> 3
+    bonusPoints = countBonusPoints score.mine
+  in
+    case compare score.mine score.opponents of
+      GT -> 6 + bonusPoints
+      LT -> 0 + bonusPoints
+      EQ -> 3 + bonusPoints
